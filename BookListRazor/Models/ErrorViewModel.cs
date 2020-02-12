d60bae3c87fe70c9ed3d1de8a242b1ffd4f0e3fc
unknown <Nathan Latino>
2020-02-12 03:30:06 +0100

    first commit

---
+++ BookListRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookListRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
